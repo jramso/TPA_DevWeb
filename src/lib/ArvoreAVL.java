8014311a9e6f0331ca934ba53ebec17563ada805
maressakaren <khsmaressa@gmail.com>
2023-10-24 09:46:27 -0300

    implements fator de balanceamento

--- src/lib/ArvoreAVL.java
+++ src/lib/ArvoreAVL.java
@@ -2,12 +2,24 @@ package lib;
 
 import java.util.Comparator;
 
-public class ArvoreAVL <T> extends ArvoreBinaria<T>{
+public class ArvoreAVL <T>extends ArvoreBinaria<T>{
+    
 
     public ArvoreAVL(Comparator<T> comparator) {
         super(comparator);
     }
+
+    protected No<T> addRecursivo( No<T> raiz, No<T> novo){
+        raiz = super.adicionaRecursivo(raiz,novo);
+        //Continuar implementando essa
+
+
+        return raiz;
+    }
+
+    public int fatorDeBalanceamento(){
+        return alturaRecursivo(this.raiz.getFilhoDireita()) - alturaRecursivo(this.raiz.getFilhoEsquerda());
     
     //Implementar métodos para efetuar o balanceamento e sobrescrever método de adicionar elemento...
-
+    }
 }
