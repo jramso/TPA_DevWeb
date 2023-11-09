cca5cb894d4358cb13269d34cf46bc15a5839133
jramso <josue.rsou2@gmail.com>
2023-11-09 14:32:34 -0300

    docs(Grafo): Adicionando JavaDoc a classe de grafo.
    docs(README): Atualizando descrição das funcionalidades do repositório

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # TPA_DevWeb
-(i) implementar bibliotecas de árvores binárias, (ii) aplicar as bibliotecas implementadas em um sistema e (iii) entender como a topologia da árvore pode influenciar na ordem de complexidade de execução dos algoritmos. 
- Link para o Relatório: https://docs.google.com/document/d/1gtNARAJqFPOfok8pzfnGMVHsehsbkCJJILFPqMyEtwA/edit?usp=sharing
+
+- TPA/1
+    - 1. implementar bibliotecas de árvores binárias
+    - 2. aplicar as bibliotecas implementadas em um sistema
+    - 3. entender como a topologia da árvore pode influenciar na ordem de complexidade de execução dos algoritmos. 
+    - 0. Link para o Relatório: https://docs.google.com/document/d/1gtNARAJqFPOfok8pzfnGMVHsehsbkCJJILFPqMyEtwA/edit?usp=sharing
+- TPA/2
+    - 1. implementar uma biblioteca de grafo.
+    - 2. aplicar as bibliotecas implementadas em um sistema
--- src/lib/Grafo.java
+++ src/lib/Grafo.java
@@ -12,18 +12,35 @@ public class Grafo {
     private List<Vertice> vertices;
     private List<Aresta> arestas;
 
+    /**
+     * Constructor - Crie um grafo com 2 listas uma de vertices outra de arestas
+     * @see Vertice
+     * @see Aresta
+     */
     public Grafo() {
         vertices = new ArrayList<>();
         arestas = new ArrayList<>();
     }
 
+    /**
+     * Este método adiciona um vértice no Grafo inserindo-o na lista de vertices.
+     * @param nome - nome do vertice (ex: V1, V2, A, B, C, "1", "2","3")
+     * @return v - retorna o vértice adicionado
+     */
     public Vertice adicionaVertice(String nome) {
         Vertice v = new Vertice(nome);
         vertices.add(v);
         return v;
     }
 
-    
+    /**
+     * SOBRECARGA : :
+     * Este método adiciona uma aresta no Grafo com peso.
+     * @param origem - ponto de inicio da aresta
+     * @param destino - ponto de destino da aresta
+     * @param peso - custo da origem até o destino
+     * @return e - retorna uma aresta
+     */
     public Aresta adicionaAresta(Vertice origem, Vertice destino, int peso) {
         Aresta e = new Aresta(origem, destino, peso);
         origem.addAdj(e);
@@ -31,7 +48,13 @@ public class Grafo {
         arestas.add(e);
         return e;
     }
-    
+    /**
+     * SOBRECARGA : :
+     * Este método adiciona uma aresta no Grafo sem peso.
+     * @param origem - ponto de inicio da aresta
+     * @param destino - ponto de destino da aresta
+     * @return e - retorna uma aresta
+     */
     public Aresta adicionaAresta(Vertice origem, Vertice destino) {
         Aresta e = new Aresta(origem, destino);
         origem.addAdj(e);
@@ -40,6 +63,12 @@ public class Grafo {
         return e;
     }    
 
+    /**
+     * Este método verifica se tem ou não uma aresta entre 2 vértices
+     * @param origem - ponto de origem da possivel aresta
+     * @param destino - ponto de destino da possivel aresta
+     * @return boolean
+     */
     public boolean temAresta(Vertice origem, Vertice destino) {
         for (Aresta e : origem.getAdj()) {
             if (e.getDestino() == destino) {
@@ -49,13 +78,19 @@ public class Grafo {
         return false;
     }
 
+    /**
+     * Este metodo retorna o peso de uma aresta entre origem e Destino se existir uma aresta
+     * @param origem - ponto de origem da possivel aresta
+     * @param destino - ponto de destino da possivel aresta
+     * @return peso da aresta
+     */
     public int getPeso(Vertice origem, Vertice destino) {
         for (Aresta e : origem.getAdj()) {
             if (e.getDestino() == destino) {
                 return e.getPeso();
             }
         }
-        return -1;
+        return 0;
     }
 
     public String toString() {
