c6ac0872dae68a3a6b80921f39732031abcf0fcf
jramso <josue.rsou2@gmail.com>
2023-12-04 14:56:37 -0300

    Feat(Main(Grafo))final test

--- src/app/Main.java
+++ src/app/Main.java
@@ -20,7 +20,7 @@ public class Main {
         GeradorDeArvores gerador = new GeradorDeArvores();
         IArvoreBinaria<Aluno> arv;*/
 
-        ComparadorAlunoPorMatricula comp = new ComparadorAlunoPorMatricula();
+        //ComparadorAlunoPorMatricula comp = new ComparadorAlunoPorMatricula();
 
        // ArvoreBinaria <Aluno> arvore = new ArvoreBinaria<Aluno>(comp);
        // No no = arvore.adicionar(aluno);
@@ -41,26 +41,42 @@ public class Main {
        
         Grafo<String> g = new Grafo<>();
 
+        Grafo<Integer> g2 = new Grafo<>();
+
         Vertice<String> v1 = g.adicionaVertice("V1");
         Vertice<String> v2 = g.adicionaVertice("V2");
         Vertice<String> v3 = g.adicionaVertice("V3");
         Vertice<String> v4 = g.adicionaVertice("V4");
+
+        Vertice<Integer> vg1 = g2.adicionaVertice(1);
+        Vertice<Integer> vg2 = g2.adicionaVertice(2);
+        Vertice<Integer> vg3 = g2.adicionaVertice(3);
+        Vertice<Integer> vg4 = g2.adicionaVertice(4);
+
 
         // Adicionar arestas
         g.adicionaAresta(v1, v2);
         g.adicionaAresta(v2, v3);
         g.adicionaAresta(v3, v1);
 
+        g2.adicionaAresta(vg1, vg2);
+        g2.adicionaAresta(vg2, vg3);
+        g2.adicionaAresta(vg3, vg4);
+
         // Imprimir o grafo
-        System.out.println("Grafo:");
+        System.out.println("Grafo 1:");
         System.out.println(g);
 
+        System.out.println("Grafo 2:");
+        System.out.println(g2);
+
+
         // Verificar se o grafo tem ciclos
-        System.out.println("Tem ciclo: " + g.temCiclo());
+        System.out.println("Grafo 1: Tem ciclo? " + g.temCiclo());
 
         // Realizar ordenação topológica
         List<Vertice<String>> ordemTopologica = g.ordenacaoTopologica();
-        System.out.println("Ordenação Topológica:");
+        System.out.println("Grafo 1: Ordenação Topológica:");
         System.out.println(ordemTopologica);
 
     }
